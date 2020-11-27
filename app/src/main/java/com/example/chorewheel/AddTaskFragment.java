83c19ead8e6e64df22da7949b412537aa55bc6cd
alpacawool <44758340+alpacawool@users.noreply.github.com>
2020-11-26 21:12:25 -0800

    Add Task Functionality: Add Task Name, Description, and Due Date

--- app/src/main/java/com/example/chorewheel/AddTaskFragment.java
+++ app/src/main/java/com/example/chorewheel/AddTaskFragment.java
@@ -3,19 +3,40 @@ package com.example.chorewheel;
 import android.os.Bundle;
 
 import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseRelation;
+import com.parse.SaveCallback;
+import com.parse.ParseUser;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class AddTaskFragment extends DialogFragment {
+
+    public static final String TAG = "AddTaskFragment";
+
+    private EditText etAddTaskName;
+    private EditText etAddTaskDate;
+    private EditText etAddTaskDescription;
+    private Button btnAddTask;
+
     public AddTaskFragment() {
         // Empty constructor required for Task Fragment
     }
     // The onCreateView method is called when Fragment should create its View object hierarchy,
-    // either dynamically or via XML layout inflation
-
     public static AddTaskFragment newInstance(String title) {
 
         AddTaskFragment frag = new AddTaskFragment();
@@ -32,10 +53,48 @@ public class AddTaskFragment extends DialogFragment {
     }
 
     // This event is triggered soon after onCreateView().
-    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
-        // Setup any handles to view objects here
-        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+        etAddTaskName = view.findViewById(R.id.etAddTaskName);
+        etAddTaskDate = view.findViewById(R.id.etdAddTaskDate);
+        etAddTaskDescription = view.findViewById(R.id.etAddTaskNotes);
+        btnAddTask = view.findViewById(R.id.btnAddTask);
+
+        // Submit task button functionality
+        btnAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create task
+                ParseObject newTask = new ParseObject("Task");
+                newTask.put("name", etAddTaskName.getText().toString());
+                newTask.put("description", etAddTaskDescription.getText().toString());
+                // TODO: Select a user for task. Default behavior: Current User
+                newTask.put("userID", ParseUser.getCurrentUser());
+                // Add Task Date (Format: MM/dd/yyy (Capitalization matters)
+                DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+                String dateString = etAddTaskDate.getText().toString();
+                try {
+                    Date dateObject = dateFormat.parse(dateString);
+                    newTask.put("dueDate", dateObject);
+                } catch (java.text.ParseException e) {
+                    e.printStackTrace();
+                }
+                newTask.put("checked", false);
+
+                // Saves the task
+                newTask.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e != null) {
+                            Log.e(TAG, "Error while adding Task! ", e);
+                        }
+                        // Close fragment
+                        dismiss();
+                    }
+                });
+
+            }
+        });
+
     }
 }
